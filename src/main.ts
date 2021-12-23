8307eab9aee2c9a9481fd1f9f42e757c30aaad47
Samuel Hübner <samu.huebner@gmail.com>
2021-12-23 22:49:52 +0100

    feat(*): Enable CORS for the API

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // enable CORS
+  app.enableCors({
+    origin: '*',
+    methods: ['POST', 'OPTIONS'],
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
